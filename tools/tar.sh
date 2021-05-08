39152770fbf2d3879919f7646d3263709aab0c25
oushu1zhangxiang1 <zhang.xiangxuan@oushu.io>
2021-05-08 11:42:31 +0800

    add some scripts

---
+++ tools/tar.sh
@@ -0,0 +1,14 @@
+压缩
+tar -czvf /path/to/file.tar.gz file
+
+解压
+tar -xzvf /path/to/file.tar.gz /path/to
+
+加密压缩
+tar -czvf - file | openssl des3 -salt -k password -out /path/to/file.tar.gz
+
+解密解压
+openssl des3 -d -k password -salt -in /path/to/file.tar.gz | tar xzf -
+————————————————
+版权声明：本文为CSDN博主「u010359663」的原创文章，遵循CC 4.0 BY-SA版权协议，转载请附上原文出处链接及本声明。
+原文链接：https://blog.csdn.net/u010359663/article/details/84923693
---
+++ tools/util.md
@@ -0,0 +1,3 @@
+### centos7安装ntfs-3g以支持NTFS格式磁盘
+
+https://blog.csdn.net/u013077984/article/details/88614261
